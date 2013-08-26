1f1921356ef88b49f6fdb6aaccd03bfd69ec0bae
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-08-26 19:36:33 +0200

    add autoinclude entry-point

--- setup.py
+++ setup.py
@@ -28,5 +28,7 @@ setup(name='collective.contentrules.mailadapter',
       ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
